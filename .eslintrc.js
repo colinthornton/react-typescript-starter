02f99dcc8baee8e976090bed94e25a96cc5bf6bb
Colin Thornton <colinthorntontx@gmail.com>
2020-05-12 09:41:20 +0900

    Update ESLint config (#4)
    * Use @typescript-eslint recommended config
    
    * Bump ecmaVersion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended', // This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
