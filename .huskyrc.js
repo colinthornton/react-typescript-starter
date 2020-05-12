66e8b2b243bca374ed40ccf503b6a8da802c55bf
Colin Thornton <colinthorntontx@gmail.com>
2020-05-12 09:48:19 +0900

    Remove pretterrc (#5)
    * Delete .prettierrc
    
    * Run linter
    
    * Clean up gitignore

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   hooks: {
-    'pre-commit': 'webpack && lint-staged', // Catch build errors by running webpack before linting
+    "pre-commit": "webpack && lint-staged", // Catch build errors by running webpack before linting
   },
 };
