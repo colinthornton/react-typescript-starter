f5324943e7170850a77c2bd1e74c1ac961a957a1
Colin Thornton <colinthorntontx@gmail.com>
2020-05-12 10:57:48 +0900

    Support Sass (#6)
    * Config webpack for scss
    
    * Add some styles

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { modules: true },
+          },
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: "/node_modules/",
